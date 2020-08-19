802d5fbfa8e7f50b6af90045796ac9c66fd2a93f
Mysta3 <myshawnestallings@gmail.com>
2020-08-19 06:19:28 -0700

    added dynamic nav menu

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -7,7 +7,7 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class()?>>
+<body <?php body_class() ?>>
     <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a></h1>
@@ -15,13 +15,13 @@
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
             <div class="site-header__menu group">
                 <nav class="main-navigation">
-                    <ul class="min-list group">
-                        <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-                        <li><a href="#">Programs</a></li>
-                        <li><a href="#">Events</a></li>
-                        <li><a href="#">Campuses</a></li>
-                        <li><a href="#">Blog</a></li>
-                    </ul>
+
+                    <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'headerMenuLocation'
+                    ))
+                    ?>
+
                 </nav>
                 <div class="site-header__util">
                     <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
