0a629b55f6197e6a887bee096c6c9ddc99d3fc0a
Mysta3 <myshawnestallings@gmail.com>
2020-08-21 07:51:33 -0700

    added automation workflow using webpack

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,21 +1,38 @@
 <?php
 
-//add styles & scripts to pages
+//add styles & scripts to pages function
 function university_files()
 {
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+
+    if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local/')) {
+        wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    } else {
+        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+        wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.e514aaae8003cfbdaea0.js'), NULL, '1.0', true);
+        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.e514aaae8003cfbdaea0.css'));
+    }
 }
 
-//generates CSS/JS scripts
-add_action('wp_enqueue_scripts', 'university_files');
+//generates scripts and uses hooks
+add_action('wp_enqueue_scripts', 'university_files'); //calls function
 
 function university_features()
 {
     add_theme_support('title-tag');
-    
 }
 add_action('after_setup_theme', 'university_features');
 
+
+function university_post_types()
+{
+    register_post_type('event', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events'
+        )
+    ));
+}
+
+add_action('init', 'university_post_types');
